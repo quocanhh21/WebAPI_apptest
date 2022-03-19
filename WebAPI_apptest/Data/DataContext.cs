47f973b648df46e9141488e293c847475375c547
admin <admin@DESKTOP-PENMKAK>
2022-03-19 17:49:48 +0700

    Add project files.

---
+++ WebAPI_apptest/Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI_apptest.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
